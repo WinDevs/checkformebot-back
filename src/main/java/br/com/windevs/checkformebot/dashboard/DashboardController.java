f4f3741061a9360c69ed6d425199d9041ee8e4b1
Rafael Prado <email@rprado.dev>
2020-06-01 00:11:44 -0300

    feat: enable cross origin access to mocked result
    Took 7 minutes

--- src/main/java/br/com/windevs/checkformebot/dashboard/DashboardController.java
+++ src/main/java/br/com/windevs/checkformebot/dashboard/DashboardController.java
@@ -1,12 +1,10 @@
 package br.com.windevs.checkformebot.dashboard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/dashboard")
 public class DashboardController {
 
@@ -34,3 +32,4 @@ public class DashboardController {
 				"}");
 	}
 }
+
